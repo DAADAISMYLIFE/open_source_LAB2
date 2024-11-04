33c551a0f6eb33d2f1f900f3111909cae6bd6a91
qkekdhd <qkekdhd@naver.com>
2024-11-04 20:32:57 +0900

    학생 수 최대로 받을 때까지 반복

--- 8_student_map/src/main.c
+++ 8_student_map/src/main.c
@@ -16,18 +16,23 @@ int main(void){
 
 	std::cout << "최대 학생 수 : ";
 	std::cin >> n;
-
-	for(int i = 0; i < n; i++){
+	
+	std::cin.ignore(); // 입력 버퍼 비우기
+	int count = 0;
+	while(count < n){
 		std::string name;
 		STUD new_stud;
-		
-		std::cin >> name >> new_stud.g_mid >> new_stud.g_fin;
+        	if (!(std::cin >> name >> new_stud.g_mid >> new_stud.g_fin)) {
+            		break; // EOF시 종료
+        	}
+
 
 		std::cout << "\"" << name <<"\"님이 ";
 
 		if(student.find(name) == std::end(student)){
 			std::cout <<"추가 되었습니다." << std::endl;
 			student.insert({name, new_stud});
+			count++;
 		}
 		else{
 			std::cout << "이미 존재합니다." << std::endl;
